f3e9e0760400b994a9a866a47a40750b9b90444d
Sean Stevenson <sean.flynn.stevenson@gmail.com>
2023-12-19 16:49:34 -0600

    Router change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
- <Routes>
+ <Routes basename={process.env.REACT_APP_URI}>
   <Route path="/" element={<Layout />} >
   
     <Route index element={<MainPage/>} />
